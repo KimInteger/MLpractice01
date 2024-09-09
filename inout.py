fbede78574904a04b5d5a8de1af2a6064e077df2
김정수 <kjs20151240@gmail.com>
2024-09-09 12:55:22 +0900

    feat | inout.py
    성 이름 나누기

--- inout.py
+++ inout.py
@@ -1,9 +1,11 @@
-my_family = input("성을 입력하세요 :")
 my_name  = input("이름을 입력하세요 : ")
 
 
-def print_name(family,name) :
-    full_name = family + " "+ name
-    print(f"나의 성은 {family}고, 나의 이름은 {name} 그래서 저의 이름은 {full_name}입니다.")
+def print_name(name) :
+    if(name) :
+        family = name[0]
+        own_name = name[1:]
+    full_name = family + own_name
+    print(f"나의 성은 '{family}'이고, 나의 이름은 '{own_name}'입니다. 그래서 저의 이름은 {full_name}입니다.")
     
-print_name(my_family,my_name)
+print_name(my_name)
