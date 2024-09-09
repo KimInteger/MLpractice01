7a4eee5dd42d450535b1f48f64bc48681bbc68b6
김정수 <kjs20151240@gmail.com>
2024-09-09 11:44:31 +0900

    feat | inout.py
    매개변수 두개를 사용하여 성과 이름을 이야기하기.

--- inout.py
+++ inout.py
@@ -1,6 +1,9 @@
-my_name = input("이름을 입력하세요 :")
+my_family = input("성을 입력하세요 :")
+my_name  = input("이름을 입력하세요 : ")
 
-def print_name(name) :
-    print(f"내 이름은 {name}입니다.")
+
+def print_name(family,name) :
+    full_name = family + " "+ name
+    print(f"나의 성은 {family}고, 나의 이름은 {name} 그래서 저의 이름은 {full_name}입니다.")
     
-print_name(my_name)
+print_name(my_family,my_name)
