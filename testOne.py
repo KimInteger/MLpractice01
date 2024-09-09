1db5e050334ddb602963aa1179905bcde96088c7
김정수 <kjs20151240@gmail.com>
2024-09-09 11:10:04 +0900

    feat | testOne.py
    Key도 ""쌍따옴표 doubleQuote를 사용

--- testOne.py
+++ testOne.py
@@ -15,3 +15,16 @@ anther_example = lambda: "예제"
 
 add = lambda x,y : x+y
 print(add(3,56))
+
+
+array = ["배성빈", "문혜림", "신지윤"]
+
+
+# key 부분도 ""을 사용함. 
+obj = {
+    "조자연" : "네이처 조",
+    "김보미" : "개굴",
+    "황재민" : "러스트"
+}
+
+print(obj["조자연"])
