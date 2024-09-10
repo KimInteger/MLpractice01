6eb160995109e33fe7256f58c3b257dff632a52f
김정수 <kjs20151240@gmail.com>
2024-09-10 10:26:27 +0900

    fix | inspect_type.py
    오타 수정

--- inspect_type.py
+++ inspect_type.py
@@ -1,11 +1,11 @@
 def convert_question(question_answer) :
     if(question_answer.isdecimal()) : 
-        insepct_type(int(question_answer))
+        inspect_type(int(question_answer))
         return
-    insepct_type(question_answer)
+    inspect_type(question_answer)
 
 
-def insepct_type (answer) :
+def inspect_type (answer) :
     if(answer) :
         answer_type = type(answer)
         print(f"적으신 것의 타입은 {answer_type}입니다.")
