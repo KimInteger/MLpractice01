ada7c7d9139850e325205433ef4c6dd2b7fe9489
김정수 <kjs20151240@gmail.com>
2024-09-10 09:56:29 +0900

    create | inspectType.py
    타입검사기 만듦

---
+++ inspectType.py
@@ -0,0 +1,16 @@
+question = input()
+
+def convert_question(question_answer) :
+    if(question_answer.isdecimal()) : 
+        insepct_type(int(question_answer))
+        return
+    insepct_type(question_answer)
+
+
+def insepct_type (answer) :
+    if(answer) :
+        answer_type = type(answer)
+        print(f"적으신 것의 타입은 {answer_type}입니다.")
+        
+        
+convert_question(question)
