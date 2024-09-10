87ae622c1288afdfd7ae89848f69bda5e9f6d917
김정수 <kjs20151240@gmail.com>
2024-09-10 10:07:56 +0900

    feat | turn_on_node.py
    subprocess를 사용하여 commandLine 사용

--- turn_on_node.py
+++ turn_on_node.py
@@ -1,10 +1,11 @@
 from inspect_type import convert_question
+import subprocess
 
 question_answer = input("뭘 하시겠어요? :")
 
 def run_what(answer) :
     if(answer == '노드') :
-        print('node')        
+        subprocess.call('node',shell=True)
         return
     elif(answer == '타입') :
         convert_question(input("아무말 렛츠게잇 : "))
